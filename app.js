9aa10ecb8d1efffe5ff5f0361ce2f872fafdd9af
Abhishek Sharma <abhishek.sharma_cs22@gla.ac.in>
2024-08-12 01:19:13 +0530

    cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 }))
